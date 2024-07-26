94ab59f534f1e1680b6bddc6a640156852bfebd8
Jorge Salas <84060615+jorgeLuisalas@users.noreply.github.com>
2024-07-26 15:46:57 +0200

    Add files via upload

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using SupermarketCheckout.Service;
+using SupermarketCheckout.Service.Services;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddSupermarketServices();
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var checkoutService = serviceProvider.GetService<ICheckoutService>();
+
+        checkoutService.Scan("GR1");
+        checkoutService.Scan("CF1");
+        checkoutService.Scan("SR1");
+        checkoutService.Scan("GR1");
+        checkoutService.Scan("GR1");
+        checkoutService.Scan("CF1");
+        checkoutService.Scan("CF1");
+
+        var total = checkoutService.Total();
+        Console.WriteLine($"Total price: £{total}");
+    }
+}
+
