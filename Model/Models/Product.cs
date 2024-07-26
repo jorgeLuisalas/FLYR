e0fec07675740dd77f276c0ed0a6a451a1488a76
Jorge Salas <84060615+jorgeLuisalas@users.noreply.github.com>
2024-07-26 15:47:57 +0200

    Add files via upload

---
+++ Model/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace SupermarketCheckout.Model.Models
+{
+    public class Product
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class CartItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
